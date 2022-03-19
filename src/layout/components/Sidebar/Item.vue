a5b0b90471170fc2827e5835f9a1cd2cba525404
Snail-Lu <snaillu@yeah.net>
2022-03-19 15:07:48 +0800

    sidebar隐藏时图标居中对齐

--- src/layout/components/Sidebar/Item.vue
+++ src/layout/components/Sidebar/Item.vue
@@ -1,30 +1,37 @@
 <script>
 export default {
-  name: 'MenuItem',
-  functional: true,
-  props: {
-    icon: {
-      type: String,
-      default: ''
-    },
-    title: {
-      type: String,
-      default: ''
-    }
-  },
-  render(h, context) {
-    const { icon, title } = context.props
-    const vnodes = []
+	name: 'MenuItem',
+	functional: true,
+	props: {
+		icon: {
+			type: String,
+			default: ''
+		},
+		title: {
+			type: String,
+			default: ''
+		}
+	},
+	render(h, context) {
+		const { icon, title } = context.props
+		const vnodes = []
 
-    if (icon) {
-      // vnodes.push(<svg-icon icon-class={icon}/>)
-      vnodes.push(<i class={icon}></i>)
-    }
+		if (icon) {
+			// vnodes.push(<svg-icon icon-class={icon}/>)
+			vnodes.push(<i class={[icon, 'sub-el-icon']} />)
+		}
 
-    if (title) {
-      vnodes.push(<span slot='title'>{(title)}</span>)
-    }
-    return vnodes
-  }
+		if (title) {
+			vnodes.push(<span slot="title">{title}</span>)
+		}
+		return vnodes
+	}
 }
 </script>
+<style scoped>
+.sub-el-icon {
+	color: currentColor;
+	width: 1em;
+	height: 1em;
+}
+</style>
